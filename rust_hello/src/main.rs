53e3e3ea59f83a4cd4fa78334ac2429b697efc4e
MuhammadHammadShah <hammad4455shah5544@gmail.com>
2026-03-27 00:41:48 +0500

    The Arise of an Expert: Setting up the Rust Fortress

---
+++ rust_hello/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let message: &str = "Hi, This is Muhammad Hammad Shah, will become a great AI architect, INSHALLAH, will become one on uni student, INSHALLAH, RUSTACEAN :D";
+    println!("{}", message)
+}
