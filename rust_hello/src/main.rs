c8fe347ea31d059e051d57dbd6cf9014032285a5
MuhammadHammadShah <hammad4455shah5544@gmail.com>
2026-03-27 16:19:43 +0500

    Sharpened the README, summoned rustc to breathe life into main.rs separately

--- rust_hello/src/main.rs
+++ rust_hello/src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
     let message: &str = "Hi, This is Muhammad Hammad Shah, will become a great AI architect, INSHALLAH, will become one on uni student, INSHALLAH, RUSTACEAN :D";
-    println!("{}", message)
+
+    println!("{}", message); // println! , ! with this it becomes a macro. Macros don't follow the same rules as normal functions.
+    // if it was a function it will be called without a !.
 }
