3136e30caa6b1cdd7a88e6b513c9588e3a3680a3
MuhammadHammadShah <hammad4455shah5544@gmail.com>
2026-03-29 00:44:41 +0500

    shadowing, variable folder

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,25 @@
 fn main() {
-    // let x = 5;
     let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
     println!("The seconds  are: {}", THREE_HOURS_IN_SECONDS);
+    print!("\n");
+
+    let y = 5;
+    println!("The value of y is: {}", y);
+    let y = 5 * 2;
+    println!("The value of y is: {}", y);
+    print!("\n");
+    {
+        let y = 5 + 100;
+        println!("The value of y is: {}", y);
+    }
+    println!("The value of y is: {}", y);
+
+    // shadowing
+    let spaces = "    ";
+    let spaces = spaces.len();
+    println!("{spaces}");
 }
