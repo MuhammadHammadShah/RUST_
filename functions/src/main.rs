5ea8c189efd79648b8fdcf65fa4af4c4fa753ec4
MuhammadHammadShah <hammad4455shah5544@gmail.com>
2026-03-30 23:36:06 +0500

    functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    another_function();
+    param_func(45);
+}
+
+fn another_function() {
+    println!("ok, now I am from another function.");
+}
+
+fn param_func(x: i32) {
+    println!("The value  of x is {x}")
 }
