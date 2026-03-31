31c8c31cf87bb261246d16971e0545686c1d6fcf
MuhammadHammadShah <hammad4455shah5544@gmail.com>
2026-04-01 00:37:07 +0500

    loops_2.0

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -4,8 +4,53 @@ fn main() {
     //
     // loop_it_infinitly()
     // continue_condition(false)
-    let value = plus_one(1);
-    println!("from MAIN FUNCTION: {value}");
+    // let value = plus_one(1);
+    // println!("from MAIN FUNCTION: {value}");
+    // labeled_loop();
+    // for_loop();
+    while_loop();
+}
+
+// while loop
+
+fn while_loop() {
+    let mut number: i32 = 10;
+
+    while number >= 0 {
+        println!("number: {number}");
+        number -= 1;
+    }
+
+    println!("Okay! that's it");
+}
+
+// for loop
+fn for_loop() {
+    for number in (1..5).rev() {
+        println!("numbers: {number}");
+    }
+    println!("Bye Bye!")
+}
+
+//Loop Labels to Disambiguate Between Multiple Loops
+fn labeled_loop() {
+    let mut count = 0;
+    'outerloop: loop {
+        println!("count: {count}");
+        let mut remaining: i32 = 10;
+        loop {
+            println!("remaining: {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'outerloop;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+    println!("count end: {count}")
 }
 
 fn plus_one(mut counter: i32) -> i32 {
